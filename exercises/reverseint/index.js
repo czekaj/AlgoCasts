03abee0843d148cb2fee0cdd594007d0ca77631d
Lucas Czekaj <lukasz@czekaj.us>
2018-02-03 13:14:17 -0800

    reverseint

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,14 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    const str = n.toString();
+    const negative = str[0] === "-";
+    let result = (negative === true ? "-" : "");
+    for (let i = str.length -1; i >= negative ? 1 : 0; i--) {
+        result += str[i];
+    }
+    return parseInt(result);
+}
 
 module.exports = reverseInt;
