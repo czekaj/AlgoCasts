447204cb6898c8eafecb44e97a0c012f81d171b5
Lucas Czekaj <lukasz@czekaj.us>
2018-02-03 14:35:23 -0800

    chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,17 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    let chunks = [];
+    for (let i=0; i<array.length; i=i+size) {
+    let newchunk = [];
+        for (let j=0;j<size;j++) {
+            if (i+j < array.length)
+                newchunk.push(array[i+j]);
+        }
+    chunks.push(newchunk);
+    }
+    return chunks;
+}
 
 module.exports = chunk;
