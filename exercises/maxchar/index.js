6ecdf11013bad79e8d3b4ed44e028cd45f181c7d
Lucas Czekaj <lukasz@czekaj.us>
2018-02-03 13:39:48 -0800

    maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let maxcount = 0;
+    let result = '';
+    let count = new Object();
+    for (let char of str) {
+        if (!isNaN(count[char])) {
+            count[char]++;
+        } else {
+            count[char] = maxcount = 1;
+            result = char;
+        }
+        if (maxcount < count[char]) {
+            maxcount = count[char];
+            result = char; 
+        }
+    }
+    return result;
+
+
+}
 
 module.exports = maxChar;
