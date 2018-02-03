914ef2784486dea5a21c2e05798c2f1d510e6002
Lucas Czekaj <lukasz@czekaj.us>
2018-02-03 15:47:18 -0800

    capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,15 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    let result = "";
+    for (let i=0; i<str.length; i++) {
+        if (i == 0 || (i-1>0 && str[i-1] == " ")) {
+            result += str[i].toUpperCase();
+        } else
+        result += str[i];
+    }
+    return result;
+}
 
 module.exports = capitalize;
